13f94314a5a6a212e119756b0d433503058a817e
jd479800 <jd479800@gmail.com>
2023-03-04 14:06:05 -0500

    Adds license question

--- index.js
+++ index.js
@@ -33,6 +33,25 @@ inquirer
       message: "Test Instructions:",
       name: "test-instructions",
     },
+    {
+      type: "list",
+      message: "License:",
+      name: "license",
+      choices: [
+        "Apache license 2.0",
+        "Boost Software License 1.0",
+        "BSD 2-clause 'Simplified' license",
+        "BSD 3-clause 'New' or 'Revised license",
+        "Creative Commons Zero v1.0 Universal",
+        "Eclipse Public License 2.0",
+        "GNU Affero General Public License v3.0",
+        "GNU General Public License v2.0",
+        "GNU Lesser General Public License v2.1",
+        "MIT",
+        "Mozilla Public License 2.0",
+        "The Unlicense"
+      ]
+    },
   ])
   .then((response) =>
     fs.writeFile("log.txt", response, (err) =>
